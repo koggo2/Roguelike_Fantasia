1b9c6a341c3ca67dfd2d00dd427a9dae9bc47e6f
junhyeon-kim <junhyeon-kim@AL01205653.local>
2018-07-31 00:54:03 +0900

    NavMeshComponents 추가. NavMeshSurface, NavMeshLink 테스트 중

--- Assets/_Scripts/Scene Manager/ModelTestSceneManager.cs
+++ Assets/_Scripts/Scene Manager/ModelTestSceneManager.cs
@@ -1,19 +1,20 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class ModelTestSceneManager : BaseSceneManager {
 
 	private GameCamera _gameCamera;
-	private TerrainReactor _terrainReactor;
 	public Transform HeroHolder;
 	public Transform EnemyHolder;
+	public Transform StageHolder;
 
-	public NavMeshData NavData;
+	private List<TerrainReactor> _terrainReactors;
 	
 	void Awake() {
 		_gameCamera = FindObjectOfType<GameCamera>();
-		_terrainReactor = FindObjectOfType<TerrainReactor>();
+		_terrainReactors = new List<TerrainReactor>();
 
 		Game.Init();
 	}
@@ -23,12 +24,28 @@ public class ModelTestSceneManager : BaseSceneManager {
 		Game.SpawnHeroes(HeroHolder);
 		Game.SpawnEnemies(EnemyHolder);
 
+		var fieldPrefab = Resources.Load<GameObject>("Prefabs/Fields/Field_1");
+		if (fieldPrefab != null) {
+			for (int i = 0; i < 2; ++i) {
+				var instanceField = Instantiate(fieldPrefab);
+				instanceField.transform.SetParent(StageHolder);
+				instanceField.transform.localPosition = new Vector3(0f, 0f, 100f * i);
+				instanceField.transform.localScale = Vector3.one;
+				instanceField.transform.localRotation = Quaternion.identity;
+				
+				instanceField.GetComponent<NavMeshSurface>().BuildNavMesh();
+				
+				var reactor = instanceField.GetComponentInChildren<TerrainReactor>(); 
+				reactor.OnHitTerrain += OnPlayerClickedPlane;
+				_terrainReactors.Add(reactor);
+			}
+		}
+
 		_gameCamera.AttachToHero(Game.GetLeaderActor());
-		_terrainReactor.OnHitTerrain += OnPlayerClickedPlane;
 	}
 
 	void OnDestroy() {
-		_terrainReactor.OnHitTerrain -= OnPlayerClickedPlane;
+		_terrainReactors.ForEach(reactor => reactor.OnHitTerrain -= OnPlayerClickedPlane);
 	}
 
 	public void OnPlayerClickedPlane(Vector3 clickedPoint) {
